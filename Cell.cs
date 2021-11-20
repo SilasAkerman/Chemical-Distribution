237b089b6c14748d0e20ab0b9e11f3f23e009490
SilasAkerman <94759651+SilasAkerman@users.noreply.github.com>
2021-11-20 20:21:34 +0100

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Numerics;
+using Raylib_cs;
+
+namespace Chemical_Diffusion
+{
+    class Cell
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        private double _a;
+        private double _b;
+        public double A 
+        { 
+            get 
+            { 
+                return _a; 
+            } 
+            set 
+            {
+                _a = Math.Clamp(value, 0, 1);
+            } 
+        }
+        public double B 
+        { 
+            get 
+            { 
+                return _b; 
+            } 
+            set 
+            { 
+                _b = Math.Clamp(value, 0, 1); 
+            } 
+        }
+
+        int row;
+        int col;
+        int size;
+
+        public Cell(int aRow, int aCol, int aSize)
+        {
+            row = aRow;
+            col = aCol;
+            size = aSize;
+
+            X = col * size;
+            Y = row * size;
+        }
+
+        public void Display()
+        {
+            int opacity = (int)Math.Floor((A - B) * 255);
+            opacity = Math.Clamp(opacity, 0, 255);
+            Color color = new Color(opacity, opacity, opacity, 255);
+            Raylib.DrawRectangle(X, Y, size, size, color);
+        }
+
+        public Cell CalculateNewCell(Cell[,] grid, double dA, double dB, double feed, double kill, double multiplier)
+        {
+            Cell cell = new Cell(row, col, size);
+            cell.A = A + (dA * LaplaceA(grid) - A * B * B + feed * (1 - A)) * multiplier;
+            cell.B = B + (dB * LaplaceB(grid) + A * B * B - (kill + feed) * B) * multiplier;
+            return cell;
+        }
+
+        private double LaplaceA(Cell[,] grid)
+        {
+            double sumA = 0;
+            sumA += -A;
+            sumA += grid[row - 1, col].A * 0.2;
+            sumA += grid[row + 1, col].A * 0.2;
+            sumA += grid[row, col + 1].A * 0.2;
+            sumA += grid[row, col - 1].A * 0.2;
+            sumA += grid[row - 1, col - 1].A * 0.05;
+            sumA += grid[row + 1, col - 1].A * 0.05;
+            sumA += grid[row - 1, col + 1].A * 0.05;
+            sumA += grid[row + 1, col + 1].A * 0.05;
+            return sumA;
+        }
+
+        private double LaplaceB(Cell[,] grid)
+        {
+            double sumB = 0;
+            sumB += -B;
+            sumB += grid[row - 1, col].B * 0.2;
+            sumB += grid[row + 1, col].B * 0.2;
+            sumB += grid[row, col + 1].B * 0.2;
+            sumB += grid[row, col - 1].B * 0.2;
+            sumB += grid[row - 1, col - 1].B * 0.05;
+            sumB += grid[row + 1, col - 1].B * 0.05;
+            sumB += grid[row - 1, col + 1].B * 0.05;
+            sumB += grid[row + 1, col + 1].B * 0.05;
+            return sumB;
+        }
+
+        public double CalculateDistance(Vector2 mouse)
+        {
+            Vector2 pos = new Vector2(X, Y);
+            return Vector2.Distance(pos, mouse);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using Raylib_cs;
+
+namespace Chemical_Diffusion
+{
+    class Program
+    {
+        const int WIDTH = 1900;
+        const int HEIGHT = 1000;
+        const int SPACING = 5;
+
+        const double DA = 1;
+        const double DB = 0.5;
+        const double FEED = 0.055;
+        const double KILL = 0.062;
+        //const double FEED = 0.0367;
+        //const double KILL = 0.0649;
+        const double MULTIPLIER = 1;
+
+        int rows;
+        int cols;
+
+        Cell[,] grid;
+        Cell[,] buffer;
+
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            myProgram.DoSomething();
+        }
+
+        public void DoSomething()
+        {
+            rows = HEIGHT / SPACING;
+            cols = WIDTH / SPACING;
+            grid = new Cell[rows, cols];
+            buffer = new Cell[rows, cols];
+
+            Raylib.InitWindow(WIDTH, HEIGHT, "Chemical Distribution");
+            Raylib.SetTargetFPS(0);
+
+            Setup();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Display();
+                for (int i = 0; i < 5; i++)
+                {
+                    Update();
+                }
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_R))
+                {
+                    Setup();
+                }
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        void Display()
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.RAYWHITE);
+            foreach (Cell cell in grid)
+            {
+                cell.Display();
+            }
+            //Raylib.DrawFPS(200, 200);
+
+            Raylib.EndDrawing();
+        }
+
+        void Update()
+        {
+            for (int row = 1; row < rows-1; row++)
+            {
+                for (int col = 1; col < cols-1; col++)
+                {
+                    buffer[row, col] = grid[row, col].CalculateNewCell(grid, DA, DB, FEED, KILL, MULTIPLIER); // Creates a whole new cell
+                }
+            }
+            //grid = buffer; // This might cause issues
+            grid = (Cell[,])buffer.Clone();
+        }
+
+        void Setup()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    grid[i, j] = new Cell(i, j, SPACING);
+                    buffer[i, j] = new Cell(i, j, SPACING);
+                    grid[i, j].A = 1;
+                    grid[i, j].B = 0;
+                    buffer[i, j].A = 1;
+                    buffer[i, j].B = 0;
+                }
+            }
+
+            //for (int i = rows / 2 - 20; i < rows / 2 + 20; i++)
+            //{
+            //    for (int j = cols / 2 - 10; j < cols / 2 + 10; j++)
+            //    {
+            //        grid[i, j].B = 1;
+            //    }
+            //}
+
+            //for (int i = rows / 2 - 10; i < rows / 2 + 10; i++)
+            //{
+            //    for (int j = cols / 2 - 10; j < cols / 2 + 10; j++)
+            //    {
+            //        grid[i, j].B = 1;
+            //    }
+            //}
+
+            //foreach (Cell cell in grid)
+            //{
+            //    if (cell.CalculateDistance(new Vector2(WIDTH / 2, HEIGHT / 2)) < 300)
+            //    {
+            //        cell.B = 1;
+            //    }
+            //}
+
+            //for (int i = 20; i < 25; i++) // top
+            //{
+            //    for (int j = 20; j < cols - 20; j++)
+            //    {
+            //        grid[i, j].B = 1;
+            //    }
+            //}
+            //for (int i = 20; i < rows - 20; i++) // right
+            //{
+            //    for (int j = cols - 25; j < cols - 20; j++)
+            //    {
+            //        grid[i, j].B = 1;
+            //    }
+            //}
+            //for (int i = rows - 25; i < rows - 20; i++) // bottom
+            //{
+            //    for (int j = 20; j < cols - 20; j++)
+            //    {
+            //        grid[i, j].B = 1;
+            //    }
+            //}
+            //for (int i = 20; i < rows - 20; i++) // left
+            //{
+            //    for (int j = 20; j < 25; j++)
+            //    {
+            //        grid[i, j].B = 1;
+            //    }
+            //}
+
+            while (!Raylib.IsKeyDown(KeyboardKey.KEY_SPACE) && !Raylib.WindowShouldClose())
+            {
+                Display();
+                if (Raylib.IsMouseButtonDown(MouseButton.MOUSE_LEFT_BUTTON))
+                {
+                    for (int row = 1; row < rows-1; row++)
+                    {
+                        for (int col = 1; col < cols-1; col++)
+                        {
+                            if (grid[row, col].CalculateDistance(Raylib.GetMousePosition()) < 10)
+                            {
+                                grid[row, col].B = 1;
+                            }
+                        }
+                    }
+                }
+                if (Raylib.IsKeyPressed(KeyboardKey.KEY_R))
+                {
+                    foreach (Cell cell in grid)
+                    {
+                        cell.B = 0;
+                    }
+                }
+            }
+        }
+    }
+}
